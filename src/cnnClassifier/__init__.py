b343102185c564e25dc659477a62790647202ab0
Luminousbyte <Pinaki.seal32@gmail.com>
2024-10-26 01:30:44 +0530

    Prepare Base Model

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -2,20 +2,20 @@ import os
 import sys
 import logging
 
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"   # Defines the logging format with timestamp, log level, module name, and message
 
-log_dir = "logs"
-log_filepath = os.path.join(log_dir,"running_logs.log")
-os.makedirs(log_dir, exist_ok=True)
+log_dir = "logs"                                              # Sets the directory where log files will be stored
+log_filepath = os.path.join(log_dir,"running_logs.log")       # Defines the file path for the log file within the log directory
+os.makedirs(log_dir, exist_ok=True)                           # Creates the logs directory if it does not already exist
 
 
 logging.basicConfig(
     level = logging.INFO,
     format = logging_str,
     handlers = [
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+        logging.FileHandler(log_filepath),  # Handler for writing log messages to the specified file
+        logging.StreamHandler(sys.stdout)   # Handler for displaying log messages in the console (stdout)
     ]
 )
 
-logger = logging.getLogger("cnnClassifierLogger")
+logger = logging.getLogger("cnnClassifierLogger")       # Creates a logger with the name "cnnClassifierLogger" for logging messages specific to this module
